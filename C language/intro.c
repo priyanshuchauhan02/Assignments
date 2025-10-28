f1ea4ea35f2711704c2f8e3f7925eaecfc5bec56
Priyanshu <priyanshuworkspace02@gmail.com>
2025-10-28 09:28:09 +0530

    c program

---
+++ C language/intro.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+main(){
+	printf("*****************************************************");
+	printf("\n*                                                   *");
+	printf("\n*\tName-> Priyanshu Chauhan                    *");
+	printf("\n*\tEmail-> priyanshuworkspace02@gmail.com      *");
+	printf("\n*\tMobile-> 9106236243                         *");
+	printf("\n*\tGender-> Male                               *");
+	printf("\n*\tAddress-> Surat, Gujarat                    *");
+	printf("\n*\tDegree-> B.E                                *");
+	printf("\n*\tCollege-> R.N.G.P.I.T                       *");
+	printf("\n*                                                   *");
+	printf("\n*****************************************************");
+}
