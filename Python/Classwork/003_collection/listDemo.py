1b526d90093a7529d4965f6debf0c82d9758ed36
Priyanshu <priyanshuworkspace02@gmail.com>
2026-03-14 09:16:45 +0530

    13-03-26

---
+++ Python/Classwork/003_collection/listDemo.py
@@ -0,0 +1,12 @@
+# l =[100,200,300,400]
+# print(l)
+# print(len(l))
+# print(type(l))
+
+l = [10,20,30,40,50,60,70,80,90]
+print(l[0])
+print(l[-2])
+print(l[2:5])
+print(l[-4:-1])
+print(l[1:8:2])
+print(l[::-1])
